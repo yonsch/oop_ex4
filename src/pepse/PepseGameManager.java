f423709849aa6996ea6eb9aecd1384888dee9741
Yonatan Schachter <yonatan.schachter@gmail.com>
2026-08-31 10:58:12 +0300

    Add everything

---
+++ src/pepse/PepseGameManager.java
@@ -0,0 +1,10 @@
+package pepse;
+
+import static pepse.world.Sky.sky;
+
+public class PepseGameManager {
+    public static void main(String[] args) {
+        System.out.println("Pepse");
+        sky();
+    }
+}
---
+++ src/pepse/world/Sky.java
@@ -0,0 +1,7 @@
+package pepse.world;
+
+public class Sky {
+    public static void sky() {
+        System.out.println("sky");
+    }
+}
